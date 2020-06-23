bfa6e27a53194eafe38070819974b6e19a731a96
Jéssica Carneiro <jessica.carneiro@thoughtworks.com>
2020-06-23 09:31:28 -0300

    Adding mongoose.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/todoapi', { useNewUrlParser: true, useUnifiedTopology: true  });
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
