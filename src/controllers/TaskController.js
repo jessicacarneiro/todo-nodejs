77cd38d9dd8d49d07ad6f6bf418c79af8b4913b5
Jéssica Carneiro <jessica.carneiro@thoughtworks.com>
2020-06-23 10:18:22 -0300

    Adding store route.

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -6,5 +6,10 @@ module.exports = {
     async index(req, res) {
         const tasks = await Task.find();
         return res.json(tasks)
+    },
+
+    async store(req, res) {
+        const task = await Task.create(req.body);
+        return res.json(task);
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const routes = express.Router();
 const TaskController = require('./controllers/TaskController');
 
 routes.get('/tasks', TaskController.index);
+routes.post('/tasks', TaskController.store);
 
 module.exports = routes;
