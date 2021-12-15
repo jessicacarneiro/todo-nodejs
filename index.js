7320d48931818ca0d58ff5e0fd011f35bc467158
Jéssica Carneiro <jessica.carneiro@hotmart.com>
2021-12-15 17:34:55 -0300

    Add logging

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const requireDir = require('require-dir');
 
 const app = express();
 app.use(express.json());
+
+app.use(morgan('dev'));
 
 mongoose.connect(
     'mongodb://localhost:27017/todoapi', 
